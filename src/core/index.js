a121c9f3a553f64d636030ff24e19d5de9d00848
gwk <guoweikai@cmcm.com>
2020-06-27 10:56:44 +0800

    源码解析

--- src/core/index.js
+++ src/core/index.js
@@ -1,6 +1,16 @@
+/*
+ * @Date: 2020-05-13 21:05:30
+ * @LastEditors: GWK
+ * @LastEditTime: 2020-06-27 10:33:30
+ * @FilePath: /vue/src/core/index.js
+ */
+// 引入 Vue 
 import Vue from './instance/index'
+//  引入初始化全局方法
 import { initGlobalAPI } from './global-api/index'
+//  判断是否是 服务端渲染
 import { isServerRendering } from 'core/util/env'
+// 
 import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
 
 initGlobalAPI(Vue)
--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -1,10 +1,22 @@
+/*
+ * @Date: 2020-05-13 21:05:30
+ * @LastEditors: GWK
+ * @LastEditTime: 2020-06-27 10:39:25
+ * @FilePath: /vue/src/core/instance/index.js
+ */ 
+// 引入初始化混入
 import { initMixin } from './init'
+// 引入状态混入
 import { stateMixin } from './state'
+//  引入渲染混入
 import { renderMixin } from './render'
+//  引入事件 混入
 import { eventsMixin } from './events'
+//  引入生命周期混入
 import { lifecycleMixin } from './lifecycle'
+// 引入警告
 import { warn } from '../util/index'
-
+// 构造器函数
 function Vue (options) {
   if (process.env.NODE_ENV !== 'production' &&
     !(this instanceof Vue)
@@ -13,7 +25,7 @@ function Vue (options) {
   }
   this._init(options)
 }
-
+//原型链上添加 init方法
 initMixin(Vue)
 stateMixin(Vue)
 eventsMixin(Vue)
